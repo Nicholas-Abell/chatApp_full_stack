1b33864bbc246b219d3711e8849748185d8562cc
Nick <darkforceyoda@gmail.com>
2023-06-05 01:28:21 -0500

    app wrapped in SessionProvider context

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
+"use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
 
-  if (!session) {
-    return (
-      <>
-        <div className="bg-red-800 h-screen flex justify-center items-center">
-          <div className="w-full text-center">
-            <button className="bg-white p-2 px-4 rounded-lg">
-              Login With Google
-            </button>
-          </div>
+  return (
+    <>
+      <div className="bg-red-800 h-screen flex justify-center items-center">
+        <div className="w-full text-center">
+          <button className="bg-white p-2 px-4 rounded-lg">
+            {session ? <p>Log-in</p> : <p>logout</p>}
+          </button>
         </div>
-      </>
-    );
-  }
-  return <div>Logged In</div>;
+      </div>
+    </>
+  );
 }
