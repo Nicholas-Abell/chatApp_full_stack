db64fad182754da05a3d7f22e16c5b7e2f7d2cc2
Nick <darkforceyoda@gmail.com>
2023-06-05 18:05:27 -0500

    Input comp created, react-hood-forms installed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,39 +1,11 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <>
-        <div className="bg-red-800 h-screen flex justify-center items-center">
-          <div className="w-full text-center">
-            <button
-              onClick={() => signIn("google")}
-              className="bg-white p-2 px-4 rounded-lg"
-            >
-              Log In
-            </button>
-            <a href="http://localhost:3000/api/auth/callback/google">Test</a>
-          </div>
-        </div>
-      </>
-    );
-  }
-  return (
+  return(
     <>
-      <div className="bg-red-800 h-screen flex justify-center items-center">
-        <div className="w-full text-center">
-          <button
-            onClick={() => signOut()}
-            className="bg-white p-2 px-4 rounded-lg"
-          >
-            Log Out
-            {session.user?.email}
-          </button>
-        </div>
-      </div>
+    <AuthForm />
     </>
-  );
+  )
 }
