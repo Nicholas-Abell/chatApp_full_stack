16b5ef14f3e3a41a814cb3b7fb1c559e111c4245
Nick <darkforceyoda@gmail.com>
2023-06-04 21:58:50 -0500

    condition home page for auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <>
-      <div className="bg-red-800 h-screen flex justify-center items-center">
-        <div className="w-full text-center">
-          <button className="bg-white p-2 px-4 rounded-lg">
-            Login With Google
-          </button>
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <>
+        <div className="bg-red-800 h-screen flex justify-center items-center">
+          <div className="w-full text-center">
+            <button className="bg-white p-2 px-4 rounded-lg">
+              Login With Google
+            </button>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+  return <div>Logged In</div>;
 }
