ebb97e5bdd04c1b5736954baeb4f7c43cf349b9d
Nick <darkforceyoda@gmail.com>
2023-06-14 11:28:33 -0500

    Sidebar, desktop/mobile, via mapping useRoutes

--- app/(site)/components/sidebar/DesktopSidebar.tsx
+++ app/(site)/components/sidebar/DesktopSidebar.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
+import useRoutes from "../../../Hooks/useRoutes";
+import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
 
-type DesktopSidebarProps = {
-    
+type DesktopSidebarProps = { currentUser?: User };
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-2 lg:pb-4 lg:flex lg:flex-col justify-between">
+      <nav className="mt-4 flex flex-col justify-between">
+        <ul role="list" className="flex flex-col items-center space-y-1">
+          {routes.map((item) => (
+            <DesktopItem
+              key={item.label}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              active={item.active}
+              onClick={item.onClick}
+            />
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
-
-const DesktopSidebar:React.FC<DesktopSidebarProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default DesktopSidebar;
--- app/(site)/components/sidebar/SideBar.tsx
+++ app/(site)/components/sidebar/SideBar.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+import getCurrentUser from "../../../actions/getCurrentUser";
 
 type SideBarProps = {
   children: React.ReactNode;
 };
 
-const SideBar: React.FC<SideBarProps> = ({ children }) => {
+const SideBar: React.FC<SideBarProps> = async ({ children }) => {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      <DesktopSidebar currentUser={currentUser} />
+      <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
