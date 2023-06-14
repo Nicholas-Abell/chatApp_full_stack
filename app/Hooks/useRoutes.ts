c153c2eb76b86bc368eb3d53ca2976aedafc9a5b
Nick <darkforceyoda@gmail.com>
2023-06-13 23:06:31 -0500

    functional useRoute hook

--- app/Hooks/useRoutes.ts
+++ app/Hooks/useRoutes.ts
@@ -1,6 +1,38 @@
 import { useMemo } from "react";
-import { useParams } from "next/navigation";
-import {HiChat} from 'react-icons/hi'
+import { useParams, usePathname } from "next/navigation";
+import { HiChat } from 'react-icons/hi';
+import { HiArrowLeft, HiUsers } from "react-icons/hi2";
+import { signOut } from 'next-auth/react';
 
+import useConversation from "./useConversations";
 
+const useRoutes = () => {
+    const pathname = usePathname();
+    const {conversationId} = useConversation();
+
+    const routes = useMemo(() => [
+        {
+            label: 'Chat',
+            href: '/conversations',
+            icon: HiChat,
+            active: pathname === '/conversations' || !!conversationId,
+        },
+        {
+            label: 'Users',
+            href: '/users',
+            icon: HiUsers,
+            active: pathname === '/users',
+        },
+        {
+            label: 'Logout',
+            href: '#',
+            icon: HiArrowLeft,
+            onCLick: () => signOut(),
+        },
+    ], [pathname, conversationId]);
+
+    return routes;
+}
+
+export default useRoutes;
 
