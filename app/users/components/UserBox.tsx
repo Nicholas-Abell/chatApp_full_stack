3aad5e05c403afd1df7095391842288c6d699818
Nick <darkforceyoda@gmail.com>
2023-07-18 10:39:57 -0500

    userBox comp

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,12 +1,44 @@
 "use client";
+import Avatar from "@/app/(site)/components/Avatar";
 import { User } from "@prisma/client";
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useCallback, useState } from "react";
 
 type UserBoxProps = {
   data: User;
 };
 
 const UserBox: React.FC<UserBoxProps> = ({ data }) => {
-  return <div>{data.email?.split('@', 1)}</div>;
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setLoading(true);
+
+    axios
+      .post("/api/conversations", { userId: data.id })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => setLoading(false));
+  }, [data, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+    >
+      <Avatar user={data} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div className="flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+          </div>
+        </div>
+      </div>
+      {data.email?.split("@", 1)}
+    </div>
+  );
 };
 export default UserBox;
